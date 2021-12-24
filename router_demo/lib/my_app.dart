3eb665c52f02e2f641c0eb00cf9851b1ea1aff51
Sergey <sergio.protchenkO@gmail.com>
2021-12-24 16:41:50 +0200

    auto_route branch

--- router_demo/lib/my_app.dart
+++ router_demo/lib/my_app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:router_demo/screens/log_in_screen.dart';
 
+/// auto_route package example
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
